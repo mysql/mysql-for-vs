a734114fccbe345175e064d523d6a7d122b89244
reggie <>
2005-04-13 14:59:36 +0000

    Added methods for Write and WriteLine.  This was necessary for compatibility with Pocket PC since it doesn't have Trace.Write and Trace.WriteLine

--- mysqlclient/Logger.cs
+++ mysqlclient/Logger.cs
@@ -32,20 +32,37 @@ namespace MySql.Data.MySqlClient
 		{
 			if (text.Length > 300)
 				text = text.Substring( 0, 300 );
+
 			string msg = String.Format("Executing command {0} with text ='{1}'",
-				Enum.GetName( typeof(DBCmd), cmd ), text );
-			Trace.WriteLine( msg );
+				cmd.ToString(), text);
+			//TODO: check this
+				//Enum.GetName( typeof(DBCmd), cmd ), text );
+			WriteLine( msg );
 		}
 
 		static public void LogException( Exception ex )
 		{
 			string msg = String.Format("EXCEPTION: " + ex.Message);
-			Trace.WriteLine( msg );
+			WriteLine( msg );
 		}
 
 		static public void LogWarning( string s )
 		{
-			Trace.WriteLine("WARNING:" + s);
+			WriteLine("WARNING:" + s);
+		}
+
+		static public void Write(string s) 
+		{
+#if !CF
+			Trace.Write(s);
+#endif
+		}
+
+		static public void WriteLine(string s) 
+		{
+#if !CF
+			Trace.WriteLine(s);
+#endif
 		}
 	}
 }
