941c568ad3cf9b3d69f483f76784be531798b864
reggie <>
2005-04-13 14:57:28 +0000

    Moved logging calls to the Logging class.  This helped compatibility with Pocket PC

--- mysqlclient/UsageAdvisor.cs
+++ mysqlclient/UsageAdvisor.cs
@@ -37,8 +37,8 @@ namespace MySql.Data.MySqlClient
 		{
 			if (! conn.Settings.UseUsageAdvisor) return;
 			LogUAHeader(null);
-			Trace.WriteLine("");
-			Trace.WriteLine("A rowset that was being accessed using SequentialAccess had to load " +
+			Logger.WriteLine("");
+			Logger.WriteLine("A rowset that was being accessed using SequentialAccess had to load " +
 				"all of its remaining columns.  This can cause performance problems.  This is most " +
 				"likely due to calling Prepare() on a command before reading all the columns of a " +
 				"rowset that is being accessed with SequentialAccess");
@@ -48,33 +48,33 @@ namespace MySql.Data.MySqlClient
 		public void ReadPartialRowSet(string cmdText, bool[] uaFieldsUsed, MySqlField[] fields)
 		{
 			LogUAHeader(cmdText);
-			Trace.WriteLine("Reason: Every column was not accessed.  Consider a more focused query.");
-			Trace.Write("Fields not accessed: ");
+			Logger.WriteLine("Reason: Every column was not accessed.  Consider a more focused query.");
+			Logger.Write("Fields not accessed: ");
 			for (int i=0; i < uaFieldsUsed.Length; i++)
 				if (! uaFieldsUsed[i])
-					Trace.Write(" " + fields[i].ColumnName);
-			Trace.WriteLine(" ");
+					Logger.Write(" " + fields[i].ColumnName);
+			Logger.WriteLine(" ");
 			LogUAFooter();
 		}
 
 		private void LogUAWarning(string cmdText, string reason) 
 		{
 			LogUAHeader(cmdText);
-			Trace.WriteLine("Reason: " + reason);
+			Logger.WriteLine("Reason: " + reason);
 			LogUAFooter();
 		}
 
 		private void LogUAHeader(string cmdText) 
 		{
-			Trace.WriteLine("USAGE ADVISOR WARNING -------------");
-			Trace.WriteLine("Host: " + conn.Settings.Server);
+			Logger.WriteLine("USAGE ADVISOR WARNING -------------");
+			Logger.WriteLine("Host: " + conn.Settings.Server);
 			if (cmdText != null && cmdText.Length > 0)
-				Trace.WriteLine("Command Text:  " + cmdText);
+				Logger.WriteLine("Command Text:  " + cmdText);
 		}
 
 		private void LogUAFooter() 
 		{
-			Trace.WriteLine("-----------------------------------");
+			Logger.WriteLine("-----------------------------------");
 		}
 	}
 }
