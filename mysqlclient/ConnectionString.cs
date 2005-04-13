4c823ba7978772e39a76afca174bf3bd1dc1254d
reggie <>
2005-04-13 15:11:15 +0000

    Cleaned up DriverType enum Added #if markers for CF compat Added driver option for selecting the type of connection

--- mysqlclient/ConnectionString.cs
+++ mysqlclient/ConnectionString.cs
@@ -32,6 +32,19 @@ namespace MySql.Data.MySqlClient
 	}
 
 	/// <summary>
+	/// Specifies the connection types supported
+	/// </summary>
+	public enum DriverType 
+	{
+		/// <summary>Use TCP/IP sockets</summary>
+		Native, 
+		/// <summary>Use client library</summary>
+		Client, 
+		/// <summary>Use MySQL embedded server</summary>
+		Emebedded
+	}
+
+	/// <summary>
 	/// Summary description for MySqlConnectionString.
 	/// </summary>
 	internal sealed class MySqlConnectionString : DBConnectionString
@@ -55,134 +68,176 @@ namespace MySql.Data.MySqlClient
 		}
 
 
+#if DESIGN
 		[Category("Connection")]
 		[Description("The name or IP address of the server to use")]
+#endif
 		public string Server 
 		{
 			get { return GetString("host"); }
 			set { keyValues["host"] = value; }
 		}
 
+#if DESIGN
 		[Category("Connection")]
 		[Description("Port to use when connecting with sockets")]
 		[DefaultValue(3306)]
-		public int Port 
+#endif
+		public uint Port 
 		{
-			get { return GetInt("port"); }
+			get { return (uint)GetInt("port"); }
 			set { keyValues["port"] = value; }
 		}
 
+#if DESIGN
 		[Category("Connection")]
 		[Description("Protocol to use for connection to MySQL")]
 		[DefaultValue(ConnectionProtocol.Sockets)]
+#endif
 		public ConnectionProtocol Protocol
 		{
 			get { return (ConnectionProtocol)keyValues["protocol"]; }
 			set { keyValues["protocol"] = value; }
 		}
 
+#if DESIGN
 		[Category("Connection")]
 		[Description("Name of pipe to use when connecting with named pipes (Win32 only)")]
+#endif
 		public string PipeName 
 		{
 			get { return GetString("pipeName"); }
 			set { keyValues["pipeName"] = value; }
 		}
 
+#if DESIGN
 		[Category("Connection")]
 		[Description("Should the connection ues compression")]
 		[DefaultValue(false)]
+#endif
 		public bool UseCompression 
 		{
 			get { return GetBool("compress"); }
 			set { keyValues["compress"] = value; }
 		}
 
+#if DESIGN
 		[Category("Connection")]
 		[Description("Database to use initially")]
 		[Editor("MySql.Data.MySqlClient.Design.DatabaseTypeEditor,MySqlClient.Design", typeof(System.Drawing.Design.UITypeEditor))]
+#endif
 		public string Database
 		{
 			get { return GetString("database"); }
 			set { keyValues["database"] = value; }
 		}
 
+#if DESIGN
 		[Category("Connection")]
 		[Description("Number of seconds to wait for the connection to succeed")]
 		[DefaultValue(15)]
+#endif
 		public int ConnectionTimeout
 		{
 			get { return GetInt("connect timeout"); }
 			set { keyValues["connect timeout"] = value; }
 		}
 
+#if DESIGN
 		[Category("Connection")]
 		[Description("Allows execution of multiple SQL commands in a single statement")]
 		[DefaultValue(true)]
+#endif
 		public bool AllowBatch 
 		{
 			get { return GetBool("allow batch"); }
 			set { keyValues["allow batch"] = value; }
 		}
 
+#if DESIGN
 		[Category("Connection")]
 		[Description("Enables output of diagnostic messages")]
 		[DefaultValue(false)]
+#endif
 		public bool Logging
 		{
 			get { return GetBool("logging"); }
 			set { keyValues["logging"] = value; }
 		}
 
+#if DESIGN
 		[Category("Connection")]
 		[Description("Name of the shared memory object to use")]
 		[DefaultValue("MYSQL")]
+#endif
 		public string SharedMemoryName 
 		{
 			get { return GetString("memname"); }
 			set { keyValues["memname"] = value; }
 		}
 
+#if DESIGN
 		[Category("Connection")]
 		[Description("Allows the use of old style @ syntax for parameters")]
 		[DefaultValue(false)]
+#endif
 		public bool UseOldSyntax 
 		{
 			get { return GetBool("oldsyntax"); }
 			set { keyValues["oldsyntax"] = value; }
 		}
+
+#if DESIGN
+		[Category("Connection")]
+		[Description("Specifies the type of driver to use for this connection")]
+		[DefaultValue(DriverType.Native)]
+#endif
+		public DriverType DriverType
+		{
+			get { return (DriverType)keyValues["driver"]; }
+			set { keyValues["driver"] = value; }
+		}
+
 		#endregion
 
 		#region Authentication Properties
 
+#if DESIGN
 		[Category("Authentication")]
 		[Description("The username to connect as")]
+#endif
 		public string UserId 
 		{
 			get { return GetString("user id"); }
 			set { keyValues["user id"] = value; }
 		}
 
+#if DESIGN
 		[Category("Authentication")]
 		[Description("The password to use for authentication")]
+#endif
 		public string Password 
 		{
 			get { return GetString("password"); }
 			set { keyValues["password"] = value; }
 		}
 
+#if DESIGN
 		[Category("Authentication")]
 		[Description("Should the connection use SSL.  This currently has no effect.")]
 		[DefaultValue(false)]
+#endif
 		public bool UseSSL
 		{
-			get { return GetBool("use ssl"); }
-			set { keyValues["use ssl"] = value; }
+			get { return GetBool("useSSL"); }
+			set { keyValues["useSSL"] = value; }
 		}
 
+#if DESIGN
 		[Category("Authentication")]
 		[Description("Show user password in connection string")]
 		[DefaultValue(false)]
+#endif
 		public bool PersistSecurityInfo 
 		{
 			get { return GetBool("persist security info"); }
@@ -192,36 +247,44 @@ namespace MySql.Data.MySqlClient
 
 		#region Pooling Properties
 
+#if DESIGN
 		[Category("Pooling")]
 		[Description("Should the connection support pooling")]
 		[DefaultValue(true)]
+#endif
 		public bool Pooling 
 		{
 			get { return GetBool("pooling"); }
 			set { keyValues["pooling"] = value; }
 		}
 
+#if DESIGN
 		[Category("Pooling")]
 		[Description("Minimum number of connections to have in this pool")]
 		[DefaultValue(0)]
+#endif
 		public int MinPoolSize 
 		{
 			get { return GetInt("min pool size"); }
 			set { keyValues["min pool size"] = value; }
 		}
 
+#if DESIGN
 		[Category("Pooling")]
 		[Description("Maximum number of connections to have in this pool")]
 		[DefaultValue(100)]
+#endif
 		public int MaxPoolSize 
 		{
 			get { return GetInt("max pool size"); }
 			set { keyValues["max pool size"] = value; }
 		}
 
+#if DESIGN
 		[Category("Pooling")]
 		[Description("Maximum number of seconds a connection should live.  This is checked when a connection is returned to the pool.")]
 		[DefaultValue(0)]
+#endif
 		public int ConnectionLifetime 
 		{
 			get { return GetInt("connect lifetime"); }
@@ -231,27 +294,34 @@ namespace MySql.Data.MySqlClient
 		#endregion
 
 		#region Other Properties
+
+#if DESIGN
 		[Category("Other")]
 		[Description("Should zero datetimes be supported")]
 		[DefaultValue(false)]
+#endif
 		public bool AllowZeroDateTime 
 		{
 			get { return GetBool("allowzerodatetime"); }
 			set { keyValues["alllowzerodatetime"] = value; }
 		}
 
+#if DESIGN
 		[Category("Other")]
 		[Description("Character set this connection should use")]
 		[DefaultValue(null)]
+#endif
 		public string CharacterSet 
 		{
 			get { return GetString("charset"); }
 			set { keyValues["charset"] = value; }
 		}
 
+#if DESIGN
 		[Category("Other")]
 		[Description("Logs inefficient database operations")]
 		[DefaultValue(false)]
+#endif
 		public bool UseUsageAdvisor 
 		{
 			get { return GetBool("usageAdvisor"); }
@@ -276,7 +346,7 @@ namespace MySql.Data.MySqlClient
 				ht.Remove("password");
 
 			foreach( string key in ht.Keys)
-				str.AppendFormat("{0}={1};", key, ht[key]);
+				str.AppendFormat(null, "{0}={1};", key, ht[key]);
 
 			if (str.Length > 0)
 				str.Remove( str.Length-1, 1 );
@@ -341,6 +411,7 @@ namespace MySql.Data.MySqlClient
 				defaults["memname"] = "MYSQL";
 				defaults["allowzerodatetime"] = false;
 				defaults["usageAdvisor"] = false;
+				defaults["driver"] = DriverType.Native;
 			}
 			return (Hashtable)defaults.Clone();
 		}
@@ -349,8 +420,20 @@ namespace MySql.Data.MySqlClient
 		{
 			switch (key.ToLower()) 
 			{
+				case "driver":
+					string d = value.ToLower();
+					if (d == "native")
+						hash["driver"] = DriverType.Native;
+					else if (d == "client")
+						hash["driver"] = DriverType.Client;
+					else if (d == "embedded")
+						hash["driver"] = DriverType.Emebedded;
+					else
+						throw new MySqlException("Unknown driver type: " + value);
+					return true;
+
 				case "usage advisor":
-				case "useUsageAdvisor":
+				case "useusageadvisor":
 					hash["usageAdvisor"] = value.ToLower() == "yes" || value.ToLower() == "true";
 					return true;
 
