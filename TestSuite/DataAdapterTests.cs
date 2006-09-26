2fa3ca903bb09ba65022335535922b29731ab8bf
rburnett <>
2006-09-26 20:36:31 +0000

    Bug #8131 Data Adapter doesn't close connection
    This bug was already fixed in 5.0.1 but we add the test case here as well.

--- TestSuite/DataAdapterTests.cs
+++ TestSuite/DataAdapterTests.cs
@@ -473,5 +473,44 @@ namespace MySql.Data.MySqlClient.Tests
 			}
 		}
 
+        /// <summary>
+        /// Bug #8131 Data Adapter doesn't close connection 
+        /// </summary>
+        [Test]
+        public void QuietOpenAndClose()
+        {
+            execSQL("DROP TABLE IF EXISTS test");
+            execSQL("CREATE TABLE test (id INT, PRIMARY KEY(id))");
+            execSQL("INSERT INTO test VALUES(1)");
+
+            try
+            {
+                MySqlConnection c = new MySqlConnection(GetConnectionString(true));
+                MySqlDataAdapter da = new MySqlDataAdapter("SELECT * FROM test", c);
+                MySqlCommandBuilder cb = new MySqlCommandBuilder(da);
+                Assert.IsTrue(c.State == ConnectionState.Closed);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                Assert.IsTrue(c.State == ConnectionState.Closed);
+                Assert.AreEqual(1, dt.Rows.Count);
+
+                dt.Rows[0][0] = 2;
+                DataRow[] rows = new DataRow[1];
+                rows[0] = dt.Rows[0];
+                da.Update(dt);
+                Assert.IsTrue(c.State == ConnectionState.Closed);
+
+                dt.Clear();
+                c.Open();
+                Assert.IsTrue(c.State == ConnectionState.Open);
+                da.Fill(dt);
+                Assert.IsTrue(c.State == ConnectionState.Open);
+                Assert.AreEqual(1, dt.Rows.Count);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
 	}
 }
