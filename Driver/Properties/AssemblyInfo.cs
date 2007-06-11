44b38a48699220a91e0c5d6baf406f427a095216
rburnett <>
2007-06-11 19:58:02 +0000

    bumped version # to 5.1.2.2

--- Driver/Properties/AssemblyInfo.cs
+++ Driver/Properties/AssemblyInfo.cs
@@ -22,6 +22,7 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
+using System.Security;
 
 //
 // General Information about an assembly is controlled through the following 
@@ -38,7 +39,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
-
+#if !CF
+[assembly: AllowPartiallyTrustedCallers()]
+#endif
 //
 // Version information for an assembly consists of the following four values:
 //
@@ -50,7 +53,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("5.1.2.1")]
+[assembly: AssemblyVersion("5.1.2.2")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
