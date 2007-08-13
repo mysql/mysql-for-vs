85548b146503d9bf31d491b1f55766de3aa513a8
rburnett <>
2007-08-13 21:05:29 +0000

    added backticks to test setup code to handle the case where the temp database name generated is invalid.

--- TestSuite/Source/BaseTest.cs
+++ TestSuite/Source/BaseTest.cs
@@ -96,13 +96,13 @@ namespace MySql.Data.MySqlClient.Tests
             rootConn.Open();
 
             // now create our databases
-            suExecSQL("CREATE DATABASE " + database0);
-            suExecSQL("CREATE DATABASE " + database1);
+            suExecSQL("CREATE DATABASE `" + database0 + "`");
+            suExecSQL("CREATE DATABASE `" + database1 + "`");
 
             // now allow our user to access them
-            suExecSQL(String.Format(@"GRANT ALL ON {0}.* to 'test'@'localhost' 
+            suExecSQL(String.Format(@"GRANT ALL ON `{0}`.* to 'test'@'localhost' 
 				identified by 'test'", database0));
-            suExecSQL(String.Format(@"GRANT ALL ON {0}.* to 'test'@'localhost' 
+            suExecSQL(String.Format(@"GRANT ALL ON `{0}`.* to 'test'@'localhost' 
 				identified by 'test'", database1));
             suExecSQL("FLUSH PRIVILEGES");
 
