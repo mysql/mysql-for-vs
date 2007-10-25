f1e8e88d59508cf52597a8d42ee2ebff4546e9b6
rburnett <>
2007-10-25 17:46:04 +0000

    bumped version numbers to 5.1.4

--- Driver/Properties/AssemblyInfo.cs
+++ Driver/Properties/AssemblyInfo.cs
@@ -53,7 +53,7 @@ using System.Security;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("5.1.3")]
+[assembly: AssemblyVersion("5.1.4")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
@@ -81,5 +81,5 @@ using System.Security;
 //       documentation for more information on this.
 //
 [assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyName("ConnectorNet")]
+//[assembly: AssemblyKeyName("ConnectorNet")]
 
