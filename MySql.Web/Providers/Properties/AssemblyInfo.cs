f0f99eb1c9022ee63593d1f4b1db72697932bfed
rburnett <>
2008-02-22 19:41:59 +0000

    updated version # to 5.2.1

--- MySql.Web/Providers/Properties/AssemblyInfo.cs
+++ MySql.Web/Providers/Properties/AssemblyInfo.cs
@@ -53,6 +53,6 @@ using System.Security;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("5.2")]
-[assembly: AssemblyFileVersion("5.2")]
+[assembly: AssemblyVersion("5.2.1")]
+[assembly: AssemblyFileVersion("5.2.1")]
 [assembly: AssemblyKeyName("ConnectorNet")]
