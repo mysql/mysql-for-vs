ab729b22cbb8d99774b9d89a0dcf817c82882f4d
rburnett <>
2008-04-15 12:54:28 +0000

    bumped version #

--- MySql.Data.Entity/Tests/Properties/AssemblyInfo.cs
+++ MySql.Data.Entity/Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MySql.Data.Tests.Edm")]
+[assembly: AssemblyTitle("MySql.Data.Entity.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("MySql.Data.Tests.Edm")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2008")]
+[assembly: AssemblyCompany("MySQL")]
+[assembly: AssemblyProduct("MySql.Data.Entity.Tests")]
+[assembly: AssemblyCopyright("Copyright © MySQL 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("5.3")]
