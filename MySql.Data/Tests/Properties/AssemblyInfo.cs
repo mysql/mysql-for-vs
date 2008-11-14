74fcec92a2784c3a03f9b7a6ab0084c82f81f55c
rburnett <>
2008-11-14 18:40:32 +0000

    - fixed problem with package registration that kept the DDEX provider from working (bug #40726).  Also bumped all version numbers to 5.2.5

--- MySql.Data/Tests/Properties/AssemblyInfo.cs
+++ MySql.Data/Tests/Properties/AssemblyInfo.cs
@@ -46,7 +46,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("5.2.4")]
+[assembly: AssemblyVersion("5.2.5")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
--- MySql.VisualStudio/Properties/AssemblyInfo.cs
+++ MySql.VisualStudio/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly : AssemblyVersion("5.2.4")]
-//[assembly: AssemblyKeyName("ConnectorNet")]
+[assembly : AssemblyVersion("5.2.5")]
+[assembly: AssemblyKeyName("ConnectorNet")]
