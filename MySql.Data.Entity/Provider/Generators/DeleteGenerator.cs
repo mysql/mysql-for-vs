ca473b88d5cbdc9751a41215ec1061d0a188a338
rburnett <>
2009-01-21 19:15:11 +0000

    updated copyright and licensing of files

--- MySql.Data.Entity/Provider/Generators/DeleteGenerator.cs
+++ MySql.Data.Entity/Provider/Generators/DeleteGenerator.cs
@@ -1,4 +1,24 @@
-﻿using System.Text;
+﻿// Copyright (C) 2008-2009 Sun Microsystems, Inc.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as published by
+// the Free Software Foundation
+//
+// There are special exceptions to the terms and conditions of the GPL 
+// as it is applied to this software. View the full text of the 
+// exception in file EXCEPTIONS in the directory of this software 
+// distribution.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+
+using System.Text;
 using System.Data.Common.CommandTrees;
 using System.Data.Metadata.Edm;
 using MySql.Data.MySqlClient;
--- MySql.Data.Entity/Tests/SetOperators.cs
+++ MySql.Data.Entity/Tests/SetOperators.cs
@@ -1,4 +1,4 @@
-// Copyright (C) 2004-2007 MySQL AB
+// Copyright (C) 2008-2009 Sun Microsystems, Inc.
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License version 2 as published by
