34cdf4135fb6cf83c2320f5b268140644fc2c9eb
rburnett <>
2009-04-21 14:07:00 +0000

    partial update of the copyright notice

--- MySql.VisualStudio/DbObjects/DataRowHelpers.cs
+++ MySql.VisualStudio/DbObjects/DataRowHelpers.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿// Copyright © 2008 MySQL AB, 2008-2009 Sun Microsystems, Inc.
+//
+// This file is part of MySQL Tools for Visual Studio.
+// MySQL Tools for Visual Studio is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public 
+// License version 2.1 as published by the Free Software Foundation
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Data;
