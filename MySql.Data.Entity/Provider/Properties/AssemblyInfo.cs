c6ebdde858123ba600dd3cac6a34e7d1f9c91eec
rburnett <>
2009-04-21 14:16:21 +0000

    partial update of the copyright notice

--- MySql.Data.Entity/Provider/Properties/AssemblyInfo.cs
+++ MySql.Data.Entity/Provider/Properties/AssemblyInfo.cs
@@ -1,4 +1,24 @@
-﻿using System.Reflection;
+﻿// Copyright © 2008 MySQL AB, 2008-2009 Sun Microsystems, Inc.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as published by
+// the Free Software Foundation
+//
+// There are special exceptions to the terms and conditions of the GPL 
+// as it is applied to this software. View the full text of the 
+// exception in file EXCEPTIONS in the directory of this software 
+// distribution.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
