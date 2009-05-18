99f2c06597625209e7318b0778b8096d2f7aa760
rburnett <>
2009-05-18 19:32:34 +0000

    bumped version to 5.1.8 and now using file linking so that we can bump the version by just changing a single file

--- TestSuite/Properties/AssemblyInfo.cs
+++ TestSuite/Properties/AssemblyInfo.cs
@@ -35,18 +35,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
-//
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("5.1.7")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
