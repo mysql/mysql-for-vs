5b0a1082b3070290cb302265b9ac9dde716924e4
rburnett <>
2009-05-18 20:21:06 +0000

    merged

--- MySql.Data/Provider/Properties/AssemblyInfo.cs
+++ MySql.Data/Provider/Properties/AssemblyInfo.cs
@@ -42,18 +42,6 @@ using System.Security;
 #if !CF
 [assembly: AllowPartiallyTrustedCallers()]
 #endif
-//
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("5.2.7")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
