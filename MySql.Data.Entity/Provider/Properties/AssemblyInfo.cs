570a15c5537a65ec89bc4d0383017a7e85705be4
rburnett <>
2009-05-19 18:39:07 +0000

    updated projects to use new versioninfo file (and a few other simple changes)

--- MySql.Data.Entity/Provider/Properties/AssemblyInfo.cs
+++ MySql.Data.Entity/Provider/Properties/AssemblyInfo.cs
@@ -41,16 +41,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("540e7b3c-bd0b-4980-96d1-5d140d303f7e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("6.0.3")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyName("ConnectorNet")]
