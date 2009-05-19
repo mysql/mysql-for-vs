973f4e55524a9b2e2bdb69c2073964bb04c04b0f
rburnett <>
2009-05-19 18:37:25 +0000

    added mysql.data.entity to our helper script

--- Installer/register.cmd
+++ Installer/register.cmd
@@ -1,8 +1,14 @@
 echo off
 gacutil /u mysql.data
 gacutil /u mysql.web
+gacutil /u mysql.data.entity
+
 gacutil /i MySql.Data\provider\bin\debug\mysql.data.dll
 installutil mysql.data\provider\bin\debug\mysql.data.dll
+
+gacutil /i MySql.Data.Entity\provider\bin\debug\mysql.data.entity.dll
+
 gacutil /i MySql.Web\providers\bin\debug\mysql.web.dll
 installutil mysql.web\providers\bin\debug\mysql.web.dll
+
 installer\binary\globalinstaller mysql.visualstudio\bin\debug\mysql.visualstudio.dll version=VS2008 debug=true
