33225ee83e9cc0ce95b22012ce4059bcd25f55a9
Reggie Burnett <reggie.burnett@oracle.com>
2011-02-14 10:45:56 -0600

    - fixed exection of command ";" to throw a query was empty exception instead of IndexOutOfRangeException   (MySQL bug #59537, Oracle bug #11766433)

--- MySql.Data/Provider/Source/command.cs
+++ MySql.Data/Provider/Source/command.cs
@@ -383,11 +383,11 @@ namespace MySql.Data.MySqlClient
 			lastInsertedId = -1;
 			CheckState();
 
-			if (cmdText == null ||
-				 cmdText.Trim().Length == 0)
+            cmdText = cmdText.Trim();
+			if (String.IsNullOrEmpty(cmdText))
 				throw new InvalidOperationException(Resources.CommandTextNotInitialized);
 
-			string sql = TrimSemicolons(cmdText);
+            string sql = cmdText.Trim(';');
 
             // now we check to see if we are executing a query that is buggy
             // in 4.1
@@ -557,18 +557,6 @@ namespace MySql.Data.MySqlClient
 
 		internal delegate void AsyncDelegate(int type, CommandBehavior behavior);
 		internal Exception thrownException;
-
-		private static string TrimSemicolons(string sql)
-		{
-			int start = 0;
-			while (sql[start] == ';')
-				start++;
-
-			int end = sql.Length - 1;
-			while (sql[end] == ';')
-				end--;
-			return sql.Substring(start, end-start+1);
-		}
 
 		internal void AsyncExecuteWrapper(int type, CommandBehavior behavior)
 		{
