a8ac17e991c1c4ac749a1bc9881adedfeee9b577
Reggie Burnett <reggie.burnett@oracle.com>
2011-11-01 11:57:39 -0500

    1. bumped the version # 2. put MyNetworkStream inside a proper namespace 3. Removed some unused methods from crypt 4. Removed tabs from a couple of other files

--- Source/MySql.Data/common/MyNetworkStream.cs
+++ Source/MySql.Data/common/MyNetworkStream.cs
@@ -23,160 +23,163 @@
 using System;
 using System.Net.Sockets;
 
-internal class MyNetworkStream : NetworkStream
+namespace MySql.Data.Common
 {
-  /// <summary>
-  /// Wrapper around NetworkStream.
-  /// 
-  /// MyNetworkStream is equivalent to NetworkStream, except 
-  /// 1. It throws TimeoutException if read or write timeout occurs, instead 
-  /// of IOException, to match behavior of other streams (named pipe and 
-  /// shared memory). This property comes handy in TimedStream.
-  ///
-  /// 2. It implements workarounds for WSAEWOULDBLOCK errors, that can start 
-  /// occuring after stream has times out. For a discussion about the CLR bug,
-  /// refer to  http://tinyurl.com/lhgpyf. This error should never occur, as
-  /// we're not using asynchronous operations, but apparerntly it does occur
-  /// directly after timeout has expired.
-  /// The workaround is hinted in the URL above and implemented like this:
-  /// For each IO operation, if it throws WSAEWOULDBLOCK, we explicitely set
-  /// the socket to Blocking and retry the operation once again.
-  /// </summary>
-  const int MaxRetryCount = 2;
-  Socket socket;
-
-  public MyNetworkStream(Socket socket, bool ownsSocket)
-    : base(socket, ownsSocket)
+  internal class MyNetworkStream : NetworkStream
   {
-    this.socket = socket;
-  }
+    /// <summary>
+    /// Wrapper around NetworkStream.
+    /// 
+    /// MyNetworkStream is equivalent to NetworkStream, except 
+    /// 1. It throws TimeoutException if read or write timeout occurs, instead 
+    /// of IOException, to match behavior of other streams (named pipe and 
+    /// shared memory). This property comes handy in TimedStream.
+    ///
+    /// 2. It implements workarounds for WSAEWOULDBLOCK errors, that can start 
+    /// occuring after stream has times out. For a discussion about the CLR bug,
+    /// refer to  http://tinyurl.com/lhgpyf. This error should never occur, as
+    /// we're not using asynchronous operations, but apparerntly it does occur
+    /// directly after timeout has expired.
+    /// The workaround is hinted in the URL above and implemented like this:
+    /// For each IO operation, if it throws WSAEWOULDBLOCK, we explicitely set
+    /// the socket to Blocking and retry the operation once again.
+    /// </summary>
+    const int MaxRetryCount = 2;
+    Socket socket;
 
-  bool IsTimeoutException(SocketException e)
-  {
+    public MyNetworkStream(Socket socket, bool ownsSocket)
+      : base(socket, ownsSocket)
+    {
+      this.socket = socket;
+    }
+
+    bool IsTimeoutException(SocketException e)
+    {
 #if CF
        return (e.NativeErrorCode == 10060);
 #else
-    return (e.SocketErrorCode == SocketError.TimedOut);
+      return (e.SocketErrorCode == SocketError.TimedOut);
 #endif
-  }
+    }
 
-  bool IsWouldBlockException(SocketException e)
-  {
+    bool IsWouldBlockException(SocketException e)
+    {
 #if CF
       return (e.NativeErrorCode == 10035);
 #else
-    return (e.SocketErrorCode == SocketError.WouldBlock);
+      return (e.SocketErrorCode == SocketError.WouldBlock);
 #endif
-  }
+    }
 
 
-  void HandleOrRethrowException(Exception e)
-  {
-    Exception currentException = e;
-    while (currentException != null)
+    void HandleOrRethrowException(Exception e)
     {
-      if (currentException is SocketException)
+      Exception currentException = e;
+      while (currentException != null)
       {
-        SocketException socketException = (SocketException)currentException;
-        if (IsWouldBlockException(socketException))
+        if (currentException is SocketException)
         {
-          // Workaround  for WSAEWOULDBLOCK
-          socket.Blocking = true;
-          // return to give the caller possibility to retry the call
+          SocketException socketException = (SocketException)currentException;
+          if (IsWouldBlockException(socketException))
+          {
+            // Workaround  for WSAEWOULDBLOCK
+            socket.Blocking = true;
+            // return to give the caller possibility to retry the call
+            return;
+          }
+          else if (IsTimeoutException(socketException))
+          {
+            throw new TimeoutException(socketException.Message, e);
+          }
+
+        }
+        currentException = currentException.InnerException;
+      }
+      throw (e);
+    }
+
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+      int retry = 0;
+      Exception exception = null;
+      do
+      {
+        try
+        {
+          return base.Read(buffer, offset, count);
+        }
+        catch (Exception e)
+        {
+          exception = e;
+          HandleOrRethrowException(e);
+        }
+      }
+      while (++retry < MaxRetryCount);
+      throw exception;
+    }
+
+    public override int ReadByte()
+    {
+      int retry = 0;
+      Exception exception = null;
+      do
+      {
+        try
+        {
+          return base.ReadByte();
+        }
+        catch (Exception e)
+        {
+          exception = e;
+          HandleOrRethrowException(e);
+        }
+      }
+      while (++retry < MaxRetryCount);
+      throw exception;
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+      int retry = 0;
+      Exception exception = null;
+      do
+      {
+        try
+        {
+          base.Write(buffer, offset, count);
           return;
         }
-        else if (IsTimeoutException(socketException))
+        catch (Exception e)
         {
-          throw new TimeoutException(socketException.Message, e);
+          exception = e;
+          HandleOrRethrowException(e);
         }
-
       }
-      currentException = currentException.InnerException;
+      while (++retry < MaxRetryCount);
+      throw exception;
     }
-    throw (e);
-  }
 
-
-  public override int Read(byte[] buffer, int offset, int count)
-  {
-    int retry = 0;
-    Exception exception = null;
-    do
+    public override void Flush()
     {
-      try
+      int retry = 0;
+      Exception exception = null;
+      do
       {
-        return base.Read(buffer, offset, count);
+        try
+        {
+          base.Flush();
+          return;
+        }
+        catch (Exception e)
+        {
+          exception = e;
+          HandleOrRethrowException(e);
+        }
       }
-      catch (Exception e)
-      {
-        exception = e;
-        HandleOrRethrowException(e);
-      }
+      while (++retry < MaxRetryCount);
+      throw exception;
     }
-    while (++retry < MaxRetryCount);
-    throw exception;
-  }
 
-  public override int ReadByte()
-  {
-    int retry = 0;
-    Exception exception = null;
-    do
-    {
-      try
-      {
-        return base.ReadByte();
-      }
-      catch (Exception e)
-      {
-        exception = e;
-        HandleOrRethrowException(e);
-      }
-    }
-    while (++retry < MaxRetryCount);
-    throw exception;
   }
-
-  public override void Write(byte[] buffer, int offset, int count)
-  {
-    int retry = 0;
-    Exception exception = null;
-    do
-    {
-      try
-      {
-        base.Write(buffer, offset, count);
-        return;
-      }
-      catch (Exception e)
-      {
-        exception = e;
-        HandleOrRethrowException(e);
-      }
-    }
-    while (++retry < MaxRetryCount);
-    throw exception;
-  }
-
-  public override void Flush()
-  {
-    int retry = 0;
-    Exception exception = null;
-    do
-    {
-      try
-      {
-        base.Flush();
-        return;
-      }
-      catch (Exception e)
-      {
-        exception = e;
-        HandleOrRethrowException(e);
-      }
-    }
-    while (++retry < MaxRetryCount);
-    throw exception;
-  }
-
 }
