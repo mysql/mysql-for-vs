dcc991a7a907cf1ff6c72a745d1019c8e96531bc
Gabriela Martinez <gabry.martinez@oracle.com>
2012-03-29 11:35:08 -0600

    Added support for Session_OnEnd event with MySql Connector/net which adds the following changes:
    - Added public boolean property EnableExpireCallback to set on or off deleting session when expired.
    This property can be set through the web configuration file using the value enableExpireCallback set to true otherwise can be set to false or ommited.
    
    - Added SessionStateItemExpireCallback property which makes the reference to the delegate to call the Session_OnEnd event when a session is abandon.
    - Initilize method reads the value in the web configuration and set the boolean property handle the ExpireCallBack logic.
    This makes that when a session is abandon and the RemoveItem method is executed the delegate of the ExpireCallBack reference invokes the session end event but only if a session was deleted and enableExpireCallback is true.
    
    - SetItemExpireCallback method sets the expireCallback delegate reference which before was not set.
    - DeserializeSessionItems: added this method to properly deserialize session items which is needed when creating the new SessionStateStoreData object used as parameter to invoke the Session_OnEnd event delegate when DeleteTimedOutSessionsWithCallback is executed.
    
    - CleanupOldSessions method: addded the logic to properly call the DeleteTimedOutSessions based on the value of EnableExpireCallback. If true then the DeleteTimedOutSessionsWithCallback is executed otherwise call DeleteTimedOutSessionsWithOutCallback.
    
    - DeleteTimedOutSessionsWithCallback: Deletes all the expired sessions and invokes the expireCallback delegates which starts the Session_OnEnd event
    
    - DeleteTimedOutSessionsWithoutCallback: Deletes all the expired sessions without invoking the expireCallback delegate which is the default behavior.
    
    - All the database commands are wrapped into transactions to properly rollback in case of failure.
    
    - Added a new checkbox for webConfiguration Dialog so the flag can be set using the UI.
    
    - Added sessiontests class to unit test project to test the Expire Callback on true and false cases.
    
    Documentation references:
    http://msdn.microsoft.com/en-us/library/aa478952.aspx
    http://msdn.microsoft.com/en-us/library/ms178586.aspx
    http://msdn.microsoft.com/en-us/library/ms178587.aspx
    http://msdn.microsoft.com/en-us/library/aa478952.aspx
    http://msdn.microsoft.com/en-us/library/system.web.sessionstate.sessionstatestoreproviderbase.setitemexpirecallback(v=

---
+++ Tests/MySql.Web.Tests/SessionTests.cs
@@ -0,0 +1,122 @@
+﻿using NUnit.Framework;
+using System;
+using MySql.Web.SessionState;
+using MySql.Data.MySqlClient;
+using System.Web.SessionState;
+using System.Threading;
+using System.Collections.Specialized;
+using System.IO;
+
+namespace MySql.Web.Tests
+{
+  [TestFixture]
+  class SessionTests : BaseWebTest
+  {           
+    string strSessionID;
+    string calledId;
+
+    [SetUp]
+    public override void Setup()
+    {
+      base.Setup();          
+    }
+
+    private byte[] Serialize(SessionStateItemCollection items)
+    {
+      MemoryStream ms = new MemoryStream();
+      BinaryWriter writer = new BinaryWriter(ms);
+      if (items != null)
+      {
+        items.Serialize(writer);
+      }
+      writer.Close();
+      return ms.ToArray();
+    }
+
+
+    private void CreateSessionData()
+    {
+      MySqlCommand cmd = new MySqlCommand();        
+      strSessionID = System.Guid.NewGuid().ToString();
+     
+      DateTime now = DateTime.Now;
+      DateTime lastHour = now.Subtract(new TimeSpan(1, 0, 0));
+           
+      SessionStateItemCollection collection = new SessionStateItemCollection();
+      collection["FirstName"] = "Some";
+      collection["LastName"] = "Name";
+      byte[] items = Serialize(collection);
+
+      string sql = @"INSERT INTO my_aspnet_sessions VALUES (
+            @sessionId, @appId, @created, @expires, @lockdate, @lockid, @timeout,
+            @locked, @items, @flags)";
+ 
+      cmd = new MySqlCommand(sql, conn);
+      cmd.Parameters.AddWithValue("@sessionId", strSessionID);
+      cmd.Parameters.AddWithValue("@appId", 1);
+      cmd.Parameters.AddWithValue("@created", lastHour);
+      cmd.Parameters.AddWithValue("@expires", lastHour);
+      cmd.Parameters.AddWithValue("@lockdate", lastHour);
+      cmd.Parameters.AddWithValue("@lockid", 1);
+      cmd.Parameters.AddWithValue("@timeout", 1);
+      cmd.Parameters.AddWithValue("@locked", 0);
+      cmd.Parameters.AddWithValue("@items", items);
+      cmd.Parameters.AddWithValue("@flags", 0);
+      cmd.ExecuteNonQuery();
+
+      // set our last run table to 1 hour ago
+      cmd.CommandText = "UPDATE my_aspnet_sessioncleanup SET LastRun=@lastHour";
+      cmd.Parameters.Clear();
+      cmd.Parameters.AddWithValue("@lastHour", lastHour);
+      cmd.ExecuteNonQuery();
+    }
+
+
+    private void SetSessionItemExpiredCallback(bool includeCallback)
+    {
+
+      calledId = null;
+      CreateSessionData();
+
+      MySqlSessionStateStore session = new MySqlSessionStateStore();
+
+      NameValueCollection config = new NameValueCollection();
+      config.Add("connectionStringName", "LocalMySqlServer");
+      config.Add("applicationName", "/");
+      config.Add("enableExpireCallback", includeCallback ? "true" : "false");      
+      session.Initialize("SessionProvTest", config);
+      if (includeCallback) session.SetItemExpireCallback(expireCallback);
+      Thread.Sleep(1000);
+      session.Dispose();
+    }
+
+    private long CountSessions()
+    {
+      return (long)MySqlHelper.ExecuteScalar(conn, "SELECT COUNT(*) FROM my_aspnet_sessions");
+    }
+
+    public void expireCallback(string id, SessionStateStoreData item)
+    {
+      calledId = id;
+    }
+
+
+    [Test]
+    public void SessionItemWithExpireCallback()
+    {
+      SetSessionItemExpiredCallback(true);
+      Assert.AreEqual(strSessionID, calledId);
+      Assert.AreEqual(0, CountSessions());
+    }
+
+
+    [Test]
+    public void SessionItemWithoutExpireCallback()
+    {
+      SetSessionItemExpiredCallback(false);
+      Assert.AreNotEqual(strSessionID, calledId);
+      Assert.AreEqual(0, CountSessions());
+    }
+  
+  }
+}
