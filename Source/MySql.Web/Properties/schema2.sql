b4f3f1e349aaf985b08d8e84c516c286720c0b01
Fernando Gonzalez Sanchez <fer.gonzalez@oracle.com>
2012-11-23 12:53:04 -0600

    Visual Studio only project (spin off of Connector/NET).

--- Source/MySql.Web/Properties/schema2.sql
+++
@@ -1,7 +0,0 @@
- ALTER TABLE mysql_Membership 
-            ADD PasswordKey char(32) AFTER Password, 
-            ADD PasswordFormat tinyint AFTER PasswordKey, 
-            CHANGE Email Email VARCHAR(128), COMMENT='2';
-            
-            
-                
