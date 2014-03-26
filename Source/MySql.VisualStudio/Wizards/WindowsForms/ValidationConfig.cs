c24f821d99e9ebe041f04e5deaebe9543276bd7a
Fernando Gonzalez Sanchez <fer.gonzalez@oracle.com>
2014-03-26 15:50:10 -0600

    Implementation of Windows Forms project template, WorkItems: - #1040, Implement a Windows Forms Template & Wizard to generate a form bound to a MySql database table. - #1042, Write Template project. - #1043, Write UI for wizard to present options and configure a MySql connection. - #1044, Implement generation of a typed dataset code. - #1045, Implement generation of bindings using ADO.NET to TypedDataset. - #1046, Implement generation of an Entity Framework model from a database. - #1047, Implement generation of bindings using EF. - #1048, Implement generation of code of the controls in the form.

---
+++ Source/MySql.VisualStudio/Wizards/WindowsForms/ValidationConfig.cs
@@ -0,0 +1,169 @@
+﻿// Copyright © 2008, 2014, Oracle and/or its affiliates. All rights reserved.
+//
+// MySQL for Visual Studio is licensed under the terms of the GPLv2
+// <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most 
+// MySQL Connectors. There are special exceptions to the terms and 
+// conditions of the GPLv2 as it is applied to this software, see the 
+// FLOSS License Exception
+// <http://www.mysql.com/about/legal/licensing/foss-exception.html>.
+//
+// This program is free software; you can redistribute it and/or modify 
+// it under the terms of the GNU General Public License as published 
+// by the Free Software Foundation; version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along 
+// with this program; if not, write to the Free Software Foundation, Inc., 
+// 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using MySql.Data.VisualStudio.SchemaComparer;
+using MySql.Data.VisualStudio.Wizards;
+
+
+namespace MySql.Data.VisualStudio.Wizards.WindowsForms
+{
+  public partial class ValidationConfig : WizardPage
+  {
+    private List<ColumnValidation> _colValidations;
+    private string _table;
+    private string _connectionString;
+
+    internal List<ColumnValidation> UserSettingsForColumnValidation {
+      get {
+        if (chkValidation.Checked) return _colValidations;
+        else return null;
+      }
+    }
+
+    public ValidationConfig()
+    {
+      InitializeComponent();
+    }
+
+    internal override void OnStarting(BaseWizardForm wizard)
+    {
+      WindowsFormsWizardForm wiz = (WindowsFormsWizardForm)wizard;
+      if ( ( _table != wiz.TableName ) || ( _connectionString != wiz.Connection.ConnectionString ) )
+      {
+        _table = wiz.TableName;
+        _connectionString = wiz.Connection.ConnectionString;
+        LoadGridColumns(wiz.Connection, _table);
+      }
+    }
+
+    private void chkValidation_CheckedChanged(object sender, EventArgs e)
+    {
+      grdColumns.Enabled = chkValidation.Checked;
+    }
+
+    private void LoadGridColumns(MySqlConnection con, string Table)
+    {
+      _table = Table;
+      _colValidations = new List<ColumnValidation>();
+      Dictionary<string,Column> columns = BaseWizard<BaseWizardForm>.GetColumnsFromTable(Table, con);
+      BindingSource binding = new BindingSource();
+      foreach (KeyValuePair<string, Column> kvp in columns)
+      {
+        ColumnValidation cv = new ColumnValidation(kvp.Value);
+        cv.ColumnType = ColumnType.Text;
+        cv.DefaultValue = "";
+        cv.MinValue = null;
+        cv.MaxValue = null;
+        cv.Required = true;
+        _colValidations.Add(cv);
+        //binding.Add(cv);
+      }
+      grdColumns.AutoGenerateColumns = false;
+      grdColumns.AutoSize = true;
+
+      //binding.DataSource = _colValidations;
+      //grdColumns.DataSource = binding;
+      DataGridViewTextBoxColumn colName = new DataGridViewTextBoxColumn();
+      colName.DataPropertyName = "Name";
+      colName.HeaderText = "ColumnName";
+      colName.Name = "colName";
+      grdColumns.Columns.Add(colName);
+
+      DataGridViewCheckBoxColumn colRequired = new DataGridViewCheckBoxColumn();
+      colRequired.DataPropertyName = "Required";
+      colRequired.HeaderText = "Required";
+      colRequired.Name = "colRequired";
+      grdColumns.Columns.Add(colRequired);
+
+      DataGridViewTextBoxColumn colDataType = new DataGridViewTextBoxColumn();
+      colDataType.DataPropertyName = "DataType";
+      colDataType.HeaderText = "Data Type";
+      colDataType.Name = "colDataType";
+      grdColumns.Columns.Add(colDataType);
+
+      DataGridViewTextBoxColumn colDefaultValue = new DataGridViewTextBoxColumn();
+      colDefaultValue.DataPropertyName = "DefaultValue";
+      colDefaultValue.HeaderText = "Default";
+      colDefaultValue.Name = "colDefaultValue";
+      grdColumns.Columns.Add(colDefaultValue);
+      
+      DataGridViewTextBoxColumn colMinValue = new DataGridViewTextBoxColumn();
+      colMinValue.DataPropertyName = "MinValue";
+      colMinValue.HeaderText = "Min Value";
+      colMinValue.Name = "colMinValue";
+      grdColumns.Columns.Add(colMinValue);
+
+      DataGridViewTextBoxColumn colMaxValue = new DataGridViewTextBoxColumn();
+      colMaxValue.DataPropertyName = "MaxValue";
+      colMaxValue.HeaderText = "Max Value";
+      colMaxValue.Name = "colMaxValue";
+      grdColumns.Columns.Add(colMaxValue);
+
+      grdColumns.DataSource = _colValidations;
+      //grdColumns.Update();
+    }
+
+    internal override bool IsValid()
+    {
+      return true;
+    }
+  }
+
+  internal class ColumnValidation
+  {
+    private Column _column;
+    private int? _maxValue;
+    private int? _minValue;
+    private bool _required;
+    private ColumnType _columnType;
+    private object _defaultValue;
+
+    internal Column Column { get { return _column; } }
+    internal int? MaxValue { get { return _maxValue; } set { _maxValue = value; } }
+    internal int? MinValue { get { return _minValue; } set { _minValue = value; } }
+    internal bool Required { get { return _required; } set { _required = value; } }
+    internal ColumnType ColumnType { get { return _columnType; } set { _columnType = value; } }
+    internal object DefaultValue { get { return _defaultValue; } set { _defaultValue = value; } }
+    internal string Name { get { return _column.ColumnName; } set { _column.ColumnName = value; } }
+    internal string DataType { get { return _column.DataType; } set { _column.DataType = value; } }
+
+    internal ColumnValidation(Column column)
+    {
+      _column = column;
+    }
+  }
+
+  internal enum ColumnType : int
+  {
+    Text = 1,       // TextBox
+    DateTime = 2    // DatePicker
+  }
+}
---
+++ Source/MySql.VisualStudio/Wizards/WindowsForms/WindowsFormsWizardForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace MySql.Data.VisualStudio.Wizards.WindowsForms
+{
+  partial class WindowsFormsWizardForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.dataAccessConfig1 = new MySql.Data.VisualStudio.Wizards.WindowsForms.DataAccessConfig();
+      this.validationConfig1 = new MySql.Data.VisualStudio.Wizards.WindowsForms.ValidationConfig();
+      this.SuspendLayout();
+      // 
+      // dataAccessConfig1
+      // 
+      this.dataAccessConfig1.Location = new System.Drawing.Point(12, 12);
+      this.dataAccessConfig1.Name = "dataAccessConfig1";
+      this.dataAccessConfig1.Size = new System.Drawing.Size(414, 393);
+      this.dataAccessConfig1.TabIndex = 4;
+      // 
+      // validationConfig1
+      // 
+      this.validationConfig1.Location = new System.Drawing.Point(12, 12);
+      this.validationConfig1.Name = "validationConfig1";
+      this.validationConfig1.Size = new System.Drawing.Size(414, 393);
+      this.validationConfig1.TabIndex = 12;
+      // 
+      // WindowsFormsWizardForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(441, 446);
+      this.Controls.Add(this.validationConfig1);
+      this.Controls.Add(this.dataAccessConfig1);
+      this.Name = "WindowsFormsWizardForm";
+      this.Text = "WizardForm";
+      this.Load += new System.EventHandler(this.WizardForm_Load);
+      this.Controls.SetChildIndex(this.dataAccessConfig1, 0);
+      this.Controls.SetChildIndex(this.btnNext, 0);
+      this.Controls.SetChildIndex(this.btnBack, 0);
+      this.Controls.SetChildIndex(this.btnCancel, 0);
+      this.Controls.SetChildIndex(this.btnFinish, 0);
+      this.Controls.SetChildIndex(this.validationConfig1, 0);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private DataAccessConfig dataAccessConfig1;
+    private ValidationConfig validationConfig1;
+  }
+}
