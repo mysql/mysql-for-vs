e7d3ff1251d128ffa98e3ec6ac7ec7808e377476
Gabriela Martinez <gabry.martinez@oracle.com>
2014-05-16 11:29:48 -0500

    Added changes in Installer project for including Visual Basic templates. Added new icons in template and Filter in Tables Selection for MVC wizard.

--- Source/MySql.VisualStudio/Wizards/Web/TablesSelection.cs
+++ Source/MySql.VisualStudio/Wizards/Web/TablesSelection.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿// Copyright © 2008, 2014, Oracle and/or its affiliates. All rights reserved.
+//
+// MySQL for Visual Studio is licensed under the terms of the GPLv2
+// <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most 
+// MySQL Connectors. There are special exceptions to the terms and 
+// conditions of the GPLv2 as it is applied to this software, see the 
+// FLOSS License Exception
+// <http://www.mysql.com/about/legal/licensing/foss-exception.html>.
+//
+// This program is free software; you can redistribute it and/or modify 
+// it under the terms of the GNU General Public License as published 
+// by the Free Software Foundation; version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along 
+// with this program; if not, write to the Free Software Foundation, Inc., 
+// 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Data;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 
@@ -22,6 +43,7 @@ namespace MySql.Data.VisualStudio.Wizards.Web
       InitializeComponent();      
       listTables.CellContentClick +=new DataGridViewCellEventHandler(listTables_CellContentClick);
       chkSelectAllTables.CheckedChanged += chkSelectAllTables_CheckedChanged;
+      txtFilter.KeyDown += txtFilter_KeyDown;
     }
 
     void chkSelectAllTables_CheckedChanged(object sender, EventArgs e)
@@ -134,6 +156,35 @@ namespace MySql.Data.VisualStudio.Wizards.Web
         {
           errorProvider1.SetError(listTables, "");
         }
+    }
+
+    private void txtFilter_KeyDown(object sender, KeyEventArgs e)
+    {
+      if (e.KeyCode == Keys.Back || e.KeyCode == Keys.Delete)
+      {
+        if (String.IsNullOrEmpty(txtFilter.Text) && (listTables.RowCount < _tables.Count))
+        {
+          _sourceTables.DataSource = _tables;
+          listTables.DataSource = _sourceTables;
+          listTables.Update();
+          FormatTablesList();
+          return;
+        }
+      }
+      if (e.KeyCode == Keys.Enter)
+      {
+        btnFilter_Click(sender, null);
+      }
+    }
+
+    private void btnFilter_Click(object sender, EventArgs e)
+    {
+      _sourceTables.DataSource = from s in _tables
+                                 where s.Name.StartsWith(txtFilter.Text.Trim())
+                                 select s;
+      listTables.DataSource = _sourceTables;
+      listTables.Update();
+      FormatTablesList();
     }
   }
 
