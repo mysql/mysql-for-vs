ea6d6f82bed75ece9b622502c829a829f9c2202c
Fernando Gonzalez Sanchez <fer.gonzalez@oracle.com>
2014-07-14 15:46:20 -0500

    Fix for MYSQLFORVS-47, Mismatch in assembly version in several assemblies in M4VS. (also clean up: removed MySql.Data folder).

--- Tests/MySql.Debugger.Tests/Properties/AssemblyInfo.cs
+++ Tests/MySql.Debugger.Tests/Properties/AssemblyInfo.cs
@@ -1,4 +1,26 @@
-﻿using System.Reflection;
+﻿// Copyright © 2004, 2014, Oracle and/or its affiliates. All rights reserved.
+//
+// MySQL for Visual Studio is licensed under the terms of the GPLv2
+// <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most 
+// MySQL Connectors. There are special exceptions to the terms and 
+// conditions of the GPLv2 as it is applied to this software, see the 
+// FLOSS License Exception
+// <http://www.mysql.com/about/legal/licensing/foss-exception.html>.
+//
+// This program is free software; you can redistribute it and/or modify 
+// it under the terms of the GNU General Public License as published 
+// by the Free Software Foundation; version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along 
+// with this program; if not, write to the Free Software Foundation, Inc., 
+// 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -22,15 +44,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6faac58c-6a6e-4a32-bded-272bef72b5db")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyKeyName("ConnectorNet")]
