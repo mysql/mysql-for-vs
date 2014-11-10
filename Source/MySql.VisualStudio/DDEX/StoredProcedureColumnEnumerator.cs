a1211994b89ee7ecdcf3b403c67f7f97763f3c48
Gabriela Martinez <gabry.martinez@oracle.com>
2014-11-10 13:49:14 -0600

    Updated the GPL banner on some of the files that had differences.

--- Source/MySql.VisualStudio/DDEX/StoredProcedureColumnEnumerator.cs
+++ Source/MySql.VisualStudio/DDEX/StoredProcedureColumnEnumerator.cs
@@ -1,4 +1,4 @@
-// Copyright © 2008, 2010, Oracle and/or its affiliates. All rights reserved.
+// Copyright © 2010, Oracle and/or its affiliates. All rights reserved.
 //
 // MySQL for Visual Studio is licensed under the terms of the GPLv2
 // <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most 
@@ -18,7 +18,7 @@
 //
 // You should have received a copy of the GNU General Public License along 
 // with this program; if not, write to the Free Software Foundation, Inc., 
-// 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA using System;
+// 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
 
 using System;
 using Microsoft.VisualStudio.Data;
--- Tests/Installer.Tests/RegressionTests.cs
+++ Tests/Installer.Tests/RegressionTests.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright © 2013 Oracle and/or its affiliates. All rights reserved.
 //
-// MySQL For Visual Studio is licensed under the terms of the GPLv2
+// MySQL for Visual Studio is licensed under the terms of the GPLv2
 // <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most 
 // MySQL Connectors. There are special exceptions to the terms and 
 // conditions of the GPLv2 as it is applied to this software, see the 
@@ -19,7 +19,6 @@
 // You should have received a copy of the GNU General Public License along 
 // with this program; if not, write to the Free Software Foundation, Inc., 
 // 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
