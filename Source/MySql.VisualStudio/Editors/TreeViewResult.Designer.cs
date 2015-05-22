d2ad0aa8d797f13b0233ab8e3871656f680d2f9d
Francisco Tirado <francisco.tirado@oracle.com>
2015-05-22 14:47:33 -0500

    MYSQLFORVS-295: Create new User Control to display data as a Tree View in the new JS editor.

---
+++ Source/MySql.VisualStudio/Editors/TreeViewResult.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace MySql.Data.VisualStudio.Editors
+{
+  partial class TreeViewResult
+  {
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.tvData = new System.Windows.Forms.TreeView();
+      this.SuspendLayout();
+      // 
+      // tvData
+      // 
+      this.tvData.Location = new System.Drawing.Point(0, 0);
+      this.tvData.Name = "tvData";
+      this.tvData.Size = new System.Drawing.Size(600, 500);
+      this.tvData.TabIndex = 0;
+      // 
+      // TreeViewResult
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(this.tvData);
+      this.Name = "TreeViewResult";
+      this.Size = new System.Drawing.Size(600, 500);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TreeView tvData;
+  }
+}
---
+++ Source/MySql.VisualStudio/Editors/TreeViewResult.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Serialization;
+using MySqlX.Shell;
+
+namespace MySql.Data.VisualStudio.Editors
+{
+  public partial class TreeViewResult : UserControl
+  {
+    public TreeViewResult()
+    {
+      InitializeComponent();
+    }
+
+    /// <summary>
+    /// Load data to the tree view
+    /// </summary>
+    /// <param name="document">Data to load</param>
+    public void SetData(DocumentResultSet document)
+    {
+      GenerateItem(document.GetData());
+    }
+
+    /// <summary>
+    /// Create tree nodes from the data received to add it to the tree view and display the information
+    /// </summary>
+    /// <param name="rows">Data to generate the tree nodes</param>
+    private void GenerateItem(List<Dictionary<string, object>> rows)
+    {
+      int rowCtr = 1;
+      string nodeId = "";
+      TreeNode[] currnodes = null;
+      foreach (Dictionary<string, object> row in rows)
+      {
+        nodeId = string.Format("row_{0}", rowCtr);
+        tvData.Nodes.Add(nodeId, string.Format("Row {0}", rowCtr));
+        foreach(KeyValuePair<string, object> column in row)
+        {
+          currnodes = tvData.Nodes.Find(nodeId, true);
+          currnodes[0].Nodes.Add(new TreeNode(column.Key, new TreeNode[] { new TreeNode(column.Value.ToString()) }));
+        }
+        rowCtr++;
+      }
+    }
+  }
+}
