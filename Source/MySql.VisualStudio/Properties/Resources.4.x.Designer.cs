720ed14ef94cba95b4e7b77a628033b0f52b19a2
Alfonso Penunuri <luis.penunuri@oracle.com>
2015-07-17 20:56:42 -0500

    Fixing previous build issue due the resources.resx file being updated by VS.

--- Source/MySql.VisualStudio/Properties/Resources.4.x.Designer.cs
+++ Source/MySql.VisualStudio/Properties/Resources.4.x.Designer.cs
@@ -22,14 +22,14 @@ namespace MySql.Data.VisualStudio.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources_4_x {
+    internal class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources_4_x() {
+        internal Resources () {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace MySql.Data.VisualStudio.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MySql.Data.VisualStudio.Properties.Resources.4.x", typeof(Resources_4_x).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MySql.Data.VisualStudio.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
