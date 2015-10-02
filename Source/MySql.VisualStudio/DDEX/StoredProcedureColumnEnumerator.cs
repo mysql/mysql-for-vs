49e3910f860307ed5f949aa609a3d7ffb69b242c
Alfonso Penunuri <luis.penunuri@oracle.com>
2015-10-02 12:27:36 -0500

    MYSQLFORVS-219 - Problems working with existing stored procedures in dataset designer
    * Created the static class "DbCommandExtensions", which holds several extension methods for the DbCommand command to get the store procedure definition and schema, in order to validate whether a stored procedure contains any "SIGNAL" commands, in which case the stored procedure execution will be by-passed so it can be executed properly without returning the error produced by the SIGNAL commands. (~\Source\MySql.VisualStudio\DDEX\DbCommandExtensions.cs)
    
    * Remove the "ROUTINE_CATALOG" identifier from the "Stored Procedure" type, since it was adding the "owner" to the stored procedure name, causing the dataset wizard to throw an error. Also, change the itemName values of the "StoredProcedureColumn" items to match the values that the dataset wizard is expecting.
    (~\Source\MySql.VisualStudio\DDEX\MySqlDataObjectSupport.xml)
    
    * Change the "EnumerateObjects" method to validate whether a stored procedure contains any "SIGNAL" command, in which case it will be by-passed to get the stored procedure schema and execute it correctly without the SIGNAL error (~\Source\MySql.VisualStudio\DDEX\StoredProcedureColumnEnumerator.cs).

--- Source/MySql.VisualStudio/DDEX/StoredProcedureColumnEnumerator.cs
+++ Source/MySql.VisualStudio/DDEX/StoredProcedureColumnEnumerator.cs
@@ -1,30 +1,31 @@
-// Copyright © 2008, 2010, Oracle and/or its affiliates. All rights reserved.
+// Copyright © 2008, 2015, Oracle and/or its affiliates. All rights reserved.
 //
 // MySQL for Visual Studio is licensed under the terms of the GPLv2
-// <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most 
-// MySQL Connectors. There are special exceptions to the terms and 
-// conditions of the GPLv2 as it is applied to this software, see the 
+// <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most
+// MySQL Connectors. There are special exceptions to the terms and
+// conditions of the GPLv2 as it is applied to this software, see the
 // FLOSS License Exception
 // <http://www.mysql.com/about/legal/licensing/foss-exception.html>.
 //
-// This program is free software; you can redistribute it and/or modify 
-// it under the terms of the GNU General Public License as published 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published
 // by the Free Software Foundation; version 2 of the License.
 //
-// This program is distributed in the hope that it will be useful, but 
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
 // for more details.
 //
-// You should have received a copy of the GNU General Public License along 
-// with this program; if not, write to the Free Software Foundation, Inc., 
-// 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA using System;
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA.
 
 using System;
 using Microsoft.VisualStudio.Data;
 using System.Data.Common;
 using Microsoft.VisualStudio.Data.AdoDotNet;
 using System.Data;
+using MySql.Data.VisualStudio.DDEX;
 
 namespace MySql.Data.VisualStudio
 {
@@ -37,60 +38,31 @@ namespace MySql.Data.VisualStudio
       try
       {
         string spName = String.Format("{0}.{1}", restrictions[1], restrictions[2]);
+        DataTable schemaDataTable;
 
         if (conn.State != ConnectionState.Open)
           conn.Open();
 
-        string[] parmRest = new string[5];
-        parmRest[1] = (string)restrictions[1];
-        parmRest[2] = (string)restrictions[2];
-        parmRest[3] = (string)restrictions[3];
-        DataTable parmTable = conn.GetSchema("Procedure Parameters", parmRest);
-
         DbCommand cmd = conn.CreateCommand();
-        cmd.CommandText = spName;
         cmd.CommandType = CommandType.StoredProcedure;
+        SetCommandParameters(conn, cmd, restrictions);
 
-        foreach (DataRow row in parmTable.Rows)
+        if (cmd.DoesStoredProcedureContainsSignals(restrictions))
         {
-          if (row["ORDINAL_POSITION"].Equals(0)) continue;
-
-          DbParameter p = cmd.CreateParameter();
-          p.ParameterName = row["PARAMETER_NAME"].ToString();
-          p.Value = GetDefaultValue(row["DATA_TYPE"].ToString());
-          switch (row["PARAMETER_MODE"].ToString())
-          {
-            case "IN":
-              p.Direction = ParameterDirection.Input;
-              break;
-            case "OUT":
-              p.Direction = ParameterDirection.Output;
-              break;
-            case "INOUT":
-              p.Direction = ParameterDirection.InputOutput;
-              break;
-          }
-          cmd.Parameters.Add(p);
+          schemaDataTable = cmd.GetSchemaDataTableFromStoredProcedureDefinition(restrictions);
         }
 
-        using (IDataReader reader = cmd.ExecuteReader(CommandBehavior.SchemaOnly))
+        else
         {
-          DataTable dt = reader.GetSchemaTable();
-          if (dt == null)
-            dt = new DataTable();
-          else
-          {
-            dt.Columns.Add(new DataColumn("RoutineName", typeof(string)));
-            foreach (DataRow row in dt.Rows)
-            {
-              row["RoutineName"] = restrictions[2];
-              string basedb = row["BaseSchemaName"] as string;
-              if (String.IsNullOrEmpty(basedb) || row["BaseSchemaName"] == DBNull.Value)
-                row["BaseSchemaName"] = cmd.Connection.Database;
-            }
-          }
-          return new AdoDotNetDataTableReader(dt);
+          schemaDataTable = cmd.GetSchemaDataTableFromStoredProcedureExecution(spName, restrictions);
         }
+
+        if (schemaDataTable == null)
+        {
+          schemaDataTable = new DataTable();
+        }
+
+        return new AdoDotNetDataTableReader(schemaDataTable);
       }
       finally
       {
@@ -103,7 +75,54 @@ namespace MySql.Data.VisualStudio
       if (dataType == "VARCHAR" || dataType == "VARBINARY" ||
           dataType == "ENUM" || dataType == "SET" || dataType == "CHAR")
         return "";
+
       return 0;
+    }
+
+    /// <summary>
+    /// This method set the parameters to the DbCommand object, which will be used by the stored procedure.
+    /// The parameters are pulled from the "restrictions" object.
+    /// </summary>
+    /// <param name="conn">
+    /// The DbConnection created previously in the parent method "EnumerateObjects"
+    /// </param>
+    /// <param name="cmd">
+    /// The DbCommand created previously in the parent method "EnumerateObjects"
+    /// </param>
+    /// <param name="restrictions">
+    /// An array of objects containing information about the database name, the schema and the sProc name (among other information).
+    /// </param>
+    private void SetCommandParameters(DbConnection conn, DbCommand cmd, object[] restrictions)
+    {
+      string[] parmRest = new string[4];
+      parmRest[0] = (string)restrictions[0];
+      parmRest[1] = (string)restrictions[1];
+      parmRest[2] = (string)restrictions[2];
+      parmRest[3] = (string)restrictions[3];
+      DataTable parmTable = conn.GetSchema("Procedure Parameters", parmRest);
+
+      foreach (DataRow row in parmTable.Rows)
+      {
+        if (row["ORDINAL_POSITION"].Equals(0)) continue;
+
+        DbParameter p = cmd.CreateParameter();
+        p.ParameterName = row["PARAMETER_NAME"].ToString();
+        p.Value = GetDefaultValue(row["DATA_TYPE"].ToString());
+        switch (row["PARAMETER_MODE"].ToString())
+        {
+          case "IN":
+            p.Direction = ParameterDirection.Input;
+            break;
+          case "OUT":
+            p.Direction = ParameterDirection.Output;
+            break;
+          case "INOUT":
+            p.Direction = ParameterDirection.InputOutput;
+            break;
+        }
+
+        cmd.Parameters.Add(p);
+      }
     }
   }
 }
