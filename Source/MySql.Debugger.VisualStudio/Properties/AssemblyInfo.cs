9320ecda4476124ced7bc56f31d17324a90c13cd
Javier Treviño <javier.trevino@oracle.com>
2016-03-17 13:39:42 -0600

    MYSQLFORVS-473 - Check if product assemblies need signing
    - Removed the assembly signing attribute from all projects. (Source\MySql.Debugger.VisualStudio\Properties\AssemblyInfo.cs, Source\MySql.Debugger\Properties\AssemblyInfo.cs, Source\MySql.Parser\Properties\AssemblyInfo.cs, Source\MySql.VisualStudio\Properties\AssemblyInfo.cs, Tests\MySql.Debugger.Tests\Properties\AssemblyInfo.cs, Tests\MySql.Parser.Tests\Properties\AssemblyInfo.cs, Tests\MySql.VisualStudio.Tests\Properties\AssemblyInfo.cs)
    - Removed signing from NGShellClient project. (NGShellClient\NGShellClientMgd\NGShellClientMgd.vcxproj, NGShellClient\cnet.snk)
    - Added copyright headers and fixed copyright information in assemblies. (NGShellClient\NGShellClientMgd\AssemblyInfo.cpp, NGShellClient\NGShellClient_Test\Properties\AssemblyInfo.cs)
    - Moved the NGShellClient solution to root, where all solution files are. Excluded the NGShellClient solution from source packaging. (NGShellClient.sln, Package.csproj)
    - Updated the ignore files list. (.gitignore)

--- Source/MySql.Debugger.VisualStudio/Properties/AssemblyInfo.cs
+++ Source/MySql.Debugger.VisualStudio/Properties/AssemblyInfo.cs
@@ -11,20 +11,19 @@
 // it under the terms of the GNU General Public License as published
 // by the Free Software Foundation; version 2 of the License.
 //
-// This program is distributed in the hope that it will be useful, but 
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
 // for more details.
 //
-// You should have received a copy of the GNU General Public License along 
-// with this program; if not, write to the Free Software Foundation, Inc., 
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MySql.Debugger.VisualStudio")]
@@ -32,20 +31,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Oracle")]
 [assembly: AssemblyProduct("MySql.Debugger.VisualStudio")]
-[assembly: AssemblyCopyright("Copyright © 2004, 2013, Oracle and/or its affiliates. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2004, 2016, Oracle and/or its affiliates. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ae059c2a-b096-440a-babc-fb376c400e23")]
-
-// Disable warning for "Use command line option '/keycontainer' or appropriate project settings instead of 'AssemblyKeyName'",
-// since we need to sign the assembly
-#pragma warning disable 1699
-[assembly: AssemblyKeyName("ConnectorNet")]
-#pragma warning restore 1699
--- Source/MySql.Parser/Properties/AssemblyInfo.cs
+++ Source/MySql.Parser/Properties/AssemblyInfo.cs
@@ -1,53 +1,44 @@
-﻿// Copyright © 2004,2010, Oracle and/or its affiliates.  All rights reserved.
+﻿// Copyright © 2004, 2016, Oracle and/or its affiliates.  All rights reserved.
 //
 // MySQL for Visual Studio is licensed under the terms of the GPLv2
-// <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most 
-// MySQL Connectors. There are special exceptions to the terms and 
-// conditions of the GPLv2 as it is applied to this software, see the 
+// <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most
+// MySQL Connectors. There are special exceptions to the terms and
+// conditions of the GPLv2 as it is applied to this software, see the
 // FLOSS License Exception
 // <http://www.mysql.com/about/legal/licensing/foss-exception.html>.
 //
-// This program is free software; you can redistribute it and/or modify 
-// it under the terms of the GNU General Public License as published 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published
 // by the Free Software Foundation; version 2 of the License.
 //
-// This program is distributed in the hope that it will be useful, but 
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
 // for more details.
 //
-// You should have received a copy of the GNU General Public License along 
-// with this program; if not, write to the Free Software Foundation, Inc., 
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
 
-using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Runtime.CompilerServices;
-using System.Security;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MySql.Parser")]
-[assembly: AssemblyDescription("ADO.Net driver for MySQL")]
+[assembly: AssemblyDescription("MySQL Parser")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Oracle")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright © 2004, 2013, Oracle and/or its affiliates. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2004, 2016, Oracle and/or its affiliates. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f4c49aa2-ce12-4b71-827c-a7f58d347007")]
-
-// Disable warning for "Use command line option '/keycontainer' or appropriate project settings instead of 'AssemblyKeyName'",
-// since we need to sign the assembly
-#pragma warning disable 1699
-[assembly: AssemblyKeyName("ConnectorNet")]
-#pragma warning restore 1699
