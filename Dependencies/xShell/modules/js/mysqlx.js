478020780405c2c7009706bb4c0a1adacfc9cbf2
Alfonso Penunuri <luis.penunuri@oracle.com>
2016-03-31 13:30:43 -0600

    MYSQLFORVS-530 - Update the xShell assemblies used in the wrapper
    - Update the assemblies used in the wrapper to match the new version released in PB2 (mysql-shell-1.0.3-release)
    
    * Update the assembly "NGShellClientMgd.dll" generated by the wrapper, in the "Dependencies" folder (~\mysql-for-vs\Dependencies\xShell\NGShellClientMgd.dll)
    * Since the MySql Shell Core assembly name was updated from "mysqlsh.dll" to "mysqlshcore.dll", update the reference name in the wix project (~\mysql-for-vs\Installer\visualstudio.wxs)
    * Update the "NGShellClientMgd" project to:
    -- Use the release VS VC++ redist. assemblies even for debugging (the debug assemblies was avoiding the proper execution of the project)
    -- Change the outputdir to match the project output, because the solution was moved to the root folder and this was causing problems when debugging.
    -- Change the "include" paths for the VC++ Linker due the changes in the xShell PB2 packages (new folders structure)
    (~\mysql-for-vs\NGShellClient\NGShellClientMgd\NGShellClientMgd.vcxproj)
    * Update the "managed_mysqlx_result" header and code files to match the changes made in the "shellclient.dll" assembly (~\mysql-for-vs\NGShellClient\NGShellClientMgd\managed_mysqlx_result.cpp & ~\mysql-for-vs\NGShellClient\NGShellClientMgd\managed_mysqlx_result.h)

--- Dependencies/xShell/modules/js/mysqlx.js
+++ Dependencies/xShell/modules/js/mysqlx.js
@@ -1,21 +1,21 @@
 /*
- * Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; version 2 of the
- * License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301  USA
- */
+   Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   The lines above are intentionally left blank
+*/
 
 exports.mysqlx = {}
 
@@ -55,69 +55,26 @@ exports.mysqlx.expr = function(expression)
   return expr_obj
 }
 
-// Data Type Constants
-exports.mysqlx.TinyInt = _F.mysqlx.Constant('DataTypes', 'TinyInt');
-exports.mysqlx.SmallInt = _F.mysqlx.Constant('DataTypes', 'SmallInt');
-exports.mysqlx.MediumInt = _F.mysqlx.Constant('DataTypes', 'MediumInt');
-exports.mysqlx.Int = _F.mysqlx.Constant('DataTypes', 'Int');
-exports.mysqlx.Integer = _F.mysqlx.Constant('DataTypes', 'Integer');
-exports.mysqlx.BigInt = _F.mysqlx.Constant('DataTypes', 'BigInt');
-exports.mysqlx.Real = _F.mysqlx.Constant('DataTypes', 'Real');
-exports.mysqlx.Float = _F.mysqlx.Constant('DataTypes', 'Float');
-exports.mysqlx.Double = _F.mysqlx.Constant('DataTypes', 'Double');
-exports.mysqlx.Date = _F.mysqlx.Constant('DataTypes', 'Date');
-exports.mysqlx.Time = _F.mysqlx.Constant('DataTypes', 'Time');
-exports.mysqlx.Timestamp = _F.mysqlx.Constant('DataTypes', 'Timestamp');
-exports.mysqlx.DateTime = _F.mysqlx.Constant('DataTypes', 'DateTime');
-exports.mysqlx.Year = _F.mysqlx.Constant('DataTypes', 'Year');
-exports.mysqlx.Bit = _F.mysqlx.Constant('DataTypes', 'Bit');
-
-// Data Type Functions
-exports.mysqlx.Text = function(length){
-	var t;
-	if (typeof(length) == 'undefined')
-		t = _F.mysqlx.Constant('DataTypes', 'Text');
-	else
-		t = _F.mysqlx.Constant('DataTypes', 'Text', length);
-	return t;
-}
-
-
-exports.mysqlx.Blob = function(length){
-	var b;
-	if (typeof(length) == 'undefined')
-		b = _F.mysqlx.Constant('DataTypes', 'Blob');
-	else
-		b = _F.mysqlx.Constant('DataTypes', 'Blob', length);
-	return b;
-}
-
-
-
-exports.mysqlx.Decimal = function(precision, scale){
-	var decimal;
-	if (typeof(precision) == 'undefined' && typeof(scale) == 'undefined')
-		decimal = _F.mysqlx.Constant('DataTypes', 'Decimal');
-	else if (typeof(scale) == 'undefined')
-		decimal = _F.mysqlx.Constant('DataTypes', 'Decimal', precision);
-	else
-		decimal = _F.mysqlx.Constant('DataTypes', 'Decimal', precision, scale);
+exports.mysqlx.dateValue = function(year, month, day, hour, minute, second)
+{
+	if (typeof(year) == 'undefined')
+		date_obj = _F.mysqlx.Date();
+	else if (typeof(month) == 'undefined')
+		date_obj = _F.mysqlx.Date(year);
+	else if (typeof(day) == 'undefined')
+		date_obj = _F.mysqlx.Date(year, month);
+	else if (typeof(hour) == 'undefined')
+		date_obj = _F.mysqlx.Date(year, month, day);
+	else if (typeof(minute) == 'undefined')
+		date_obj = _F.mysqlx.Date(year, month, day, hour);
+	else if (typeof(second) == 'undefined')
+		date_obj = _F.mysqlx.Date(year, month, day, hour, minute);
+	else 
+		date_obj = _F.mysqlx.Date(year, month, day, hour, minute, second);
 	
-	return decimal;
+  return date_obj
 }
 
-exports.mysqlx.Numeric = function(precision, scale){
-	var numeric;
-	if (typeof(precision) == 'undefined' && typeof(scale) == 'undefined')
-		numeric = _F.mysqlx.Constant('DataTypes', 'Numeric');
-	else if (typeof(scale) == 'undefined')
-		numeric = _F.mysqlx.Constant('DataTypes', 'Numeric', precision);
-	else
-		numeric = _F.mysqlx.Constant('DataTypes', 'Numeric', precision, scale);
-	
-	return numeric;
-}
-
-
-// Index Type Constants
-exports.mysqlx.IndexUnique = _F.mysqlx.Constant('IndexTypes', 'IndexUnique');
+// Constants
+exports.mysqlx.Type = _F.mysqlx.Type();
+exports.mysqlx.IndexType = _F.mysqlx.IndexType();
