51a542fbd8cda47006f5262f2a8be6af3e523d52
Jose Ramirez <jose.m.ramirez@oracle.com>
2017-03-14 21:28:52 -0600

    Bump version to 1.2.7
    - Bump version to 1.2.7.
    - Updated Release Notes.

--- Source/MySql.VisualStudio/Properties/VersionInfo.cs
+++ Source/MySql.VisualStudio/Properties/VersionInfo.cs
@@ -37,4 +37,4 @@ using System.Security;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.2.6")]
+[assembly: AssemblyVersion("1.2.7")]
